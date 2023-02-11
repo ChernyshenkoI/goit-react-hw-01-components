69ef07ee8c0838fbf7111b3e3a7548971d85ba6b
Iryna C <106169218+ChernyshenkoI@users.noreply.github.com>
2023-02-11 18:54:07 +0200

    finished react components

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types';
+import { FriendListItem } from './FriendListItem';
+import css from './Friends.module.css';
+
+export function FriendList({ friends }) {
+  return (
+    <ul className={css.FriendList}>
+      {friends.map(({ avatar, name, isOnline, id }) => (
+        <FriendListItem
+          key={id}
+          avatar={avatar}
+          name={name}
+          isOnline={isOnline}
+        />
+      ))}
+    </ul>
+  );
+}
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.exact({
+      avatar: PropTypes.string.isRequired,
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+    })
+  ),
+};
